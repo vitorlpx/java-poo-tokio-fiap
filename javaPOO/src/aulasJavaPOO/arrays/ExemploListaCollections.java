a7c83be4ad5095596ab1190b1c109371bffc42d3
vitorlpx <lopessccp.3k@gmail.com>
2023-06-22 16:33:12 -0300

    Arrays e listas no Java.

--- javaPOO/src/aulasJavaPOO/arrays/ExemploListaCollections.java
+++ javaPOO/src/aulasJavaPOO/arrays/ExemploListaCollections.java
@@ -8,17 +8,30 @@ public class ExemploListaCollections {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		List<String> lista = new ArrayList<String>();
-		lista.add("Pedro");
-		lista.add("Camota");
-		lista.add("Dandara");
-		lista.add(null);
-		lista.add("Dandara");
-		
-		for (String list: lista) {
-			System.out.println("Nome dos usuários: " + list);
-		}
+//		List<String> lista = new ArrayList<String>();
+//		lista.add("Pedro");
+//		lista.add("Camota");
+//		lista.add("Dandara");
+//		lista.add(null);
+//		lista.add("Dandara");
+//		
+//		for (String list: lista) {
+//			System.out.println("Nome dos usuários: " + list);
+//		}
 	
+		//-----
+		
+		List<ClasseUsuario> usuarios = new ArrayList<ClasseUsuario>();
+		ClasseUsuario diegao = new ClasseUsuario("Diegão", "dieguinho");
+		
+		usuarios.add(diegao);
+		
+		for (ClasseUsuario usuario: usuarios) {
+			System.out.println("Nome dos usuários: " + usuario.getNome());
+			System.out.println("-----------------");
+		}
+		
+		
 	}
 
 }
